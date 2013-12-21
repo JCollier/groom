c5544d0b3aad5bafea339e149a3ee53ec766f69d
Joe <root@ubuntu.(none)>
2013-12-21 11:23:56 -0800

    Editing htaccess, database config, and config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://192.168.1.104/groom/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
---
+++ index2.php
@@ -0,0 +1,4 @@
+<?php
+    echo( 'nano /var/www/codeigniter/application/controllers/welcome.php' );
+    echo( 'https://www.digitalocean.com/community/articles/how-to-install-codeigniter-on-an-ubuntu-12-04-cloud-server' );
+?>
